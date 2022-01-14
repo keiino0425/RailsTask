42468b1def17b44a40127f4efbb25e0d17eb09c4
keiino <ineedayy0425@gmail.com>
2022-01-14 11:41:50 +0900

    validate model of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :username, presence: true
+    validates :email, uniqueness: true, presence: true
+    validates :password_digest, presence: true, length: { minimum: 8 }, allow_nil: true
 end
