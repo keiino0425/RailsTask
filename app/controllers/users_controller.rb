fcab82215385e6cb4be249c30ad7b4d151046989
keiino <ineedayy0425@gmail.com>
2022-01-15 05:44:46 +0900

    controller of new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   def index
   end
-  
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params.require(:user).permit(:username, :email, :password_digest))
+    if @user.save
+      flash[:notice] = "ユーザーを新規登録しました"
+      redirect_to :users
+    else
+      render "new"
+    end
   end
 
   def show
