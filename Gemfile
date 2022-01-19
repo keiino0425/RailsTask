3856858975bf09667bb530a4cc628e81f5401124
keiino <ineedayy0425@gmail.com>
2022-01-19 16:49:21 +0900

    upload userimage

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+#画像アップロード
+gem 'carrierwave'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
+    mount_uploader :userimage, UserimageUploader
+
     validates :username, presence: true
     validates :email, uniqueness: true, presence: true
     validates :password_digest, presence: true, length: { minimum: 8 }, allow_nil: true
